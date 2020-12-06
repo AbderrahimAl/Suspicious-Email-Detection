9151da08baed1f11c300f5c2b1b8dba3a7fabcf3
abdellahaghlalou <abdellah-aghlalou@outlook.fr>
2020-12-06 13:51:11 +0100

    connecting to BD

--- Suspicious-Email-Detection/src/system/Register.java
+++ Suspicious-Email-Detection/src/system/Register.java
@@ -2,6 +2,10 @@ package system;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Register")
 public class Register extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	public static final String VUE = "/Suspicious-Email-Detection/WebContent/WEB-INF/Register.jsp"; 
+	public static final String VUE = "/WEB-INF/Register.jsp"; 
 	public static final String CHAMP_EMAIL = "email";
     public static final String CHAMP_PASS = "motdepasse";
     public static final String CHAMP_CONF = "confirmation";
@@ -38,7 +42,21 @@ public class Register extends HttpServlet {
 	        String motDePasse = request.getParameter( CHAMP_PASS );
 	        String confirmation = request.getParameter( CHAMP_CONF );
 	        String nom = request.getParameter( CHAMP_NOM );
-	        
+	        try{
+				Class.forName( "com.mysql.cj.jdbc.Driver" );
+			} catch( ClassNotFoundException e) {
+				
+			}
+			
+			String url = "jdbc:mysql://localhost:3306/sysemail?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	    	String utilisateur = "javaEE";
+	    	String motDepasse = "123456789*" ;
+	    	try (Connection connexion = DriverManager.getConnection( url,utilisateur, motDePasse );){
+	    		Statement statement = connexion.createStatement();
+	    		int result=statement.executeUpdate("INSERT INTO users (email, mot_de_passe, nom) VALUES ("+"'"+email+"'"+","+"'"+motDepasse+"'"+","+"'"+nom+"'"+"); ");
+	    	} catch (SQLException e) {
+				
+			}
 		
 	}
 
