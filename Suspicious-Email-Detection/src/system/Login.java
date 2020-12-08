57e8c75fc130df4a687782ff9b87ca8519e7fb50
KouhouMed <46538442+KouhouMed@users.noreply.github.com>
2020-12-08 03:41:59 +0100

    User checking in Login servlet

--- Suspicious-Email-Detection/src/system/Login.java
+++ Suspicious-Email-Detection/src/system/Login.java
@@ -7,13 +7,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import javax.servlet.*;
+import java.io.*;
+
 /**
  * Servlet implementation class Login
  */
 @WebServlet("/Login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	public static final String VUE = "/WEB-INF/Login.jsp"; 
+	public static final String VUE_LOGIN = "/WEB-INF/Login.jsp"; 
+	public static final String VUE_INBOX = "/WEB-INF/inbox.jsp"; 
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,16 +26,30 @@ public class Login extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+		this.getServletContext().getRequestDispatcher( VUE_LOGIN ).forward( request, response );
+		
+		response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+		
+		String email = request.getParameter("email");
+        String pass = request.getParameter("pass");
+        
+        if(ValidateLogin.checkUser(email, pass))
+        {
+            RequestDispatcher rs = request.getRequestDispatcher(VUE_INBOX);
+            rs.forward(request, response);
+        }
+        else
+        {
+           out.println("Username or Password incorrect");
+           RequestDispatcher rs = request.getRequestDispatcher(VUE_LOGIN);
+           rs.include(request, response);
+        }
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
